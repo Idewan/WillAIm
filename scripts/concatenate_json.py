1a5ea18edb60ff6e8ac848546fc10ea363db6105
Idewan <ild2105@columbia.edu>
2022-11-06 01:17:29 -0400

    Add poem2img script

--- scripts/concatenate_json.py
+++ scripts/concatenate_json.py
@@ -7,19 +7,19 @@ def main():
         "poems": []
         }
 
-    f1 = open('data/poem/1.json')
+    f1 = open('data/poem/sub/1.json')
     data1 = json.load(f1)['poems']
     f1.close()
 
-    f2 = open('data/poem/2.json')
+    f2 = open('data/poem/sub/2.json')
     data2 = json.load(f2)['poems']
     f2.close()
     
-    f3 = open('data/poem/3.json')
+    f3 = open('data/poem/sub/3.json')
     data3 = json.load(f3)['poems']
     f3.close()
     
-    f4 = open('data/poem/4.json')
+    f4 = open('data/poem/sub/4.json')
     data4 = json.load(f4)['poems']
     f4.close()
 
--- scripts/poem2img.py
+++ scripts/poem2img.py
@@ -1,5 +1,100 @@
 from diffusers import StableDiffusionPipeline
+from torch import autocast
+from tqdm import tqdm
 
 import torch
+import random
+import json
 
 
+class Model():
+
+    def __init__(self, model_id):
+        self.pipe = StableDiffusionPipeline.from_pretrained(model_id,
+            revision="fp16",
+            torch_dtype=torch.float16, 
+            use_auth_token=True
+        ).to("cuda")
+
+class Prompt():
+
+    def __init__(self):
+        self.prompt_supplement = [
+            "An artistic and dream-like represenation of how it feels for",
+            "An abstract painting of the symbols in",
+            "A realistic painting by a dutch master of the symbols",
+            "An impressionist painting of the symbols in",
+            "An impressionist painting of the metaphors in ",
+            "A painting of emotions and metaphors evoked in",
+            ""
+            ]
+        self.num_prompt_supp = len(self.prompt_supplement)
+
+    def clean_prompt(self, prompt):
+        """
+
+        """
+        prompt = prompt.replace('\r', '')
+        prompt = prompt.replace('\n', '')
+
+        return prompt
+    
+    def get_prompts(self, poem):
+        """
+        
+        """
+        supplement_1_ind = random.randrange(0, self.num_prompt_supp)
+        supplement_2_ind = random.randrange(0, self.num_prompt_supp)
+
+        prompt_1_supp = self.prompt_supplement[supplement_1_ind]
+        prompt_2_supp = self.prompt_supplement[supplement_2_ind]
+        
+        prompt_1 = prompt_1_supp + "\"" + poem + "\""
+        prompt_2 = prompt_2_supp + "\"" + poem + "\""
+
+        return [prompt_1, prompt_2]
+
+if __name__ == '__main__':
+    # Store image location, prompt and poem together
+    data = {
+        "info": "Stores image location, prompt used to generate, and poem together",
+        "poem2img": []
+        }
+
+    model_id = "CompVis/stable-diffusion-v1-4"
+
+    ldm_model = Model(model_id)
+    prompt_gen = Prompt()
+
+    f1 = open("data/poem/poem.json")
+    data_poem = json.load(f1)["poems"]
+    f1.close()
+
+    for sub_data in tqdm(data_poem):
+        temp_poem = sub_data['poem']
+        c_poem = temp_poem.encode("ascii", "ignore")
+        c_poem = temp_poem.decode()
+
+        poem = prompt_gen.clean_prompt(c_poem)
+        prompts = prompt_gen.get_prompts(poem)
+
+        for i in range(2):
+
+            with autocast("cuda"):
+                image = ldm_model.pipe(prompts[i], guidance_scale=7.5).images[0]
+
+            img_path = f"data/image/{sub_data['ID']}/{i}.png"
+            image.save(img_path)
+
+            data["poem2img"].append({
+                "id":sub_data['ID'],
+                "poem":c_poem,
+                "prompt":prompts[i],
+                "img_path":img_path,
+                "keywords":sub_data['keywords']
+            })
+    
+    with open("data/poem2img.json", "w") as f:
+        json.dump(data, f)
+    
+    print("Done!")
