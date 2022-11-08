14235fb84ff4df5c3d8b2de1dd6b287309b7f240
Idewan <ild2105@columbia.edu>
2022-11-07 19:49:14 -0500

    Add test for more data more structure

--- models/eval/clip_scores.py
+++ models/eval/clip_scores.py
@@ -21,10 +21,10 @@ class CLIPEval():
         text = clip.tokenize([poem[:300], base_poem[:300]]).to(self.device)
 
         with torch.no_grad():
-            image_features = model.encode_image(image)
-            text_features = model.encode_text(text)
+            image_features = self.model.encode_image(image)
+            text_features = self.model.encode_text(text)
 
-            logits_per_image, logits_per_text = model(image, text)
+            logits_per_image, logits_per_text = self.model(image, text)
             probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
             result["Predicted"] = probs[0][0].item()
